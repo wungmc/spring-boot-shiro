affce24d951106fdbae07c36399c7b14512658b2
wung <wungcc@gmail.com>
2018-09-19 17:43:12 +0800

    add ActiveMq

---
+++ spring-boot-shiro-service/src/main/java/com/wung/shiro/activemq/MessageConsumer.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C), 2011-2018.
+ */
+package com.wung.shiro.activemq;
+
+/**
+ * @author wung 2018/9/19.
+ */
+public class MessageConsumer {
+}
---
+++ spring-boot-shiro-web/src/test/java/com/wung/ActiveMqTest.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C), 2011-2018.
+ */
+package com.wung;
+
+/**
+ * @author wung 2018/9/19.
+ */
+public class ActiveMqTest {
+}
